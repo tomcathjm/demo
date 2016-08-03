a4e62a2f1ea0f922ae28f64b62d8238295574fa2
hjm_nba@163.com <hjm_nba@163.com>
2016-08-04 00:51:09 +0800

    demo
    Signed-off-by: hjm_nba@163.com <hjm_nba@163.com>

--- src/main/java/com/example/beggar/myapplication/MainActivity.java
+++ src/main/java/com/example/beggar/myapplication/MainActivity.java
@@ -10,10 +10,6 @@ import butterknife.OnClick;
 
 public class MainActivity extends Activity {
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
